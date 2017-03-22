57160cc4335b2657513d0dcbd09f21e29dfe8da1
zhang <523213189@qq.com>
2017-03-23 00:30:47 +0800

    init commit

---
+++ Android/app/src/main/java/com/zhang/door/DeviceControlActivity.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zhang.door;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+
+/**
+ * For a given BLE device, this Activity provides the user interface to connect, display data,
+ * and display GATT services and characteristics supported by the device.  The Activity
+ * communicates with {@code BluetoothLeService}, which in turn interacts with the
+ * Bluetooth LE API.
+ */
+public class DeviceControlActivity extends Activity{
+    private final static String TAG = DeviceControlActivity.class.getSimpleName();
+
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private BluetoothLeService mBluetoothLeService;
+    private boolean mConnected = false;
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+
+            Log.e(TAG, "mBluetoothLeService is okay");
+            // Automatically connects to the device upon successful start-up initialization.
+            //mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+            findViewById(R.id.btnDoor).setEnabled(false);
+        }
+    };
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                Log.e(TAG, "Only gatt, just wait");
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                invalidateOptionsMenu();
+
+            }else if(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action))
+            {
+                mConnected = true;
+//                ShowDialog();
+                findViewById(R.id.btnDoor).setEnabled(true);
+                Log.e(TAG, "In what we need");
+                invalidateOptionsMenu();
+                mBluetoothLeService.WriteValue("451\r\n");
+                Toast.makeText(DeviceControlActivity.this, "开门！", Toast.LENGTH_SHORT).show();
+                mBluetoothLeService.disconnect();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.gatt_services_characteristics);
+
+        final Intent intent = getIntent();
+        mDeviceName = "451";
+        mDeviceAddress = "00:15:83:00:77:AF";
+
+        // Sets up UI references.
+
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        Log.d(TAG, "Try to bindService=" + bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE));
+
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+
+
+        findViewById(R.id.btnDoor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBluetoothLeService.connect(mDeviceAddress);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(mGattUpdateReceiver);
+        unbindService(mServiceConnection);
+        if(mBluetoothLeService != null)
+        {
+            mBluetoothLeService.close();
+            mBluetoothLeService = null;
+        }
+        Log.d(TAG, "We are in destroy");
+    }
+
+
+    private void ShowDialog()
+    {
+        Toast.makeText(this, "连接上啦！", Toast.LENGTH_SHORT).show();
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(BluetoothDevice.ACTION_UUID);
+        return intentFilter;
+    }
+}
---
+++ STM32/USER/main.c
@@ -0,0 +1,44 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "timer.h"
+#include "remote.h"
+#include <math.h>
+#include <string.h>
+#include "joypad.h" 
+
+int main(void)
+{	
+	uart_init(115200);
+	delay_init();	    //延时函数初始化	  
+	LED_Init();		  	//初始化与LED连接的硬件接口
+	
+	printf("at+sleep\r\n");
+	while (1)
+	{
+		delay_ms(10);
+		while (!(USART_RX_STA&0x8000))
+		{
+			continue;
+		}
+		USART_RX_STA = 0;
+		if ((USART_RX_BUF[0] != '4')
+			||(USART_RX_BUF[1] != '5')
+			||(USART_RX_BUF[2] != '1'))
+		{
+			continue;
+		}
+		LED = !LED;
+		DOOR = 1;
+		USART_RX_STA = 0;
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		DOOR = 0;
+	}
+
+	
+}
