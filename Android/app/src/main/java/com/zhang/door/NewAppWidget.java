a4cc666623e09ee88366b52a40376802b0d05960
zhang <523213189@qq.com>
2017-03-23 22:14:08 +0800

    ver 1.0 almost finished

---
+++ Android/app/src/main/java/com/zhang/door/NewAppWidget.java
@@ -0,0 +1,75 @@
+package com.zhang.door;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private BluetoothLeService mBluetoothLeService;
+    private boolean mConnected = false;
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+    private final static String TAG = DeviceControlActivity.class.getSimpleName();
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+
+//        Intent gattServiceIntent = new Intent(context, BluetoothLeService.class);
+        Intent intentservice = new Intent();
+        intentservice.setClass(context, AppWidgetService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intentservice, 0);
+        views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+//        context.startService(intentservice);
+//        context.startService(new Intent(context, AppWidgetService.class));
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+}
+
